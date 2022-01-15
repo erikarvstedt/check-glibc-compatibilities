ded0051bed58ddf1ba97b46ce20f5533d25e4494
Erik Arvstedt <erik.arvstedt@gmail.com>
2022-01-15 14:32:11 +0100

    simplify flake

--- flake.nix
+++ flake.nix
@@ -5,10 +5,9 @@
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
   in {
-    defaultApp.${system} = { type = "app"; program = toString self.packages.${system}.get-revisions; };
-
-    packages.${system}.get-revisions = pkgs.writers.writeBash "get-revisions" ''
-      exec ${pkgs.ruby_3_0}/bin/ruby ./get-revisions.rb
+    packages.${system}.get-revisions = pkgs.writeScriptBin "get-revisions" ''
+      exec ${pkgs.ruby_3_0}/bin/ruby ${./get-revisions.rb}
     '';
+    defaultPackage.${system} = self.packages.${system}.get-revisions;
   };
 }
